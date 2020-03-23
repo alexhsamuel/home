1da1a1bbf43eed63d7bbc74de96677f8bb8d927c
Alex Samuel <alex@alexsamuel.net>
2020-03-23 00:41:09 -0400

    Clean up customize stuff.

--- dotfiles/init.el
+++ dotfiles/init.el
@@ -432,17 +432,3 @@
 )
 
 
-;; =========
-;; customize
-;; =========
-
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '
-;;  '(package-selected-packages
-;;    (quote
-;;     (use-package mmm-mode python-mode flycheck-rust dockerfile-mode lua-mode cargo multiple-cursors flycheck-pyflakes racer rust-mode pug-mode scss-mode vue-mode flx-ido projectile markdown-mode yaml-mode haskell-mode flycheck))))
- 
