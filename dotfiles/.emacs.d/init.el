3dd707766217b983374178eb79b6ae80f9144fe3
Alex Samuel <alex@alexsamuel.net>
2023-05-06 23:23:09 -0700

    Doesn't belong here.

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -480,10 +480,6 @@
 (use-package rust-mode
   :config
   (add-hook 'rust-mode-hook 'cargo-minor-mode)
-
-  (add-hook 'python-mode-hook 'flycheck-mode)
-  (add-to-list 'flycheck-disabled-checkers 'python-flake8)
-  (add-to-list 'flycheck-disabled-checkers 'python-pylint)
 )
 
 
